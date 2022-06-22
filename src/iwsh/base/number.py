64460e03becedcc7985ad10de3251dd9451cd4ca
shubham singh <ishubhamsingh2e@gmail.com>
2022-06-22 15:54:28 +0530

    fixed data conversion

--- src/iwsh/base/number.py
+++ src/iwsh/base/number.py
@@ -4,18 +4,25 @@ import numpy as np
 from google.protobuf.json_format import MessageToDict
 
 from sklearn.svm import SVC
+import site
 
 
 class number:
-    def __init__(self, landmark, hand: int) -> None:
-        self.__MODEL = pickle.load("../model/number.pkl")
-        self.landmark: dict = MessageToDict(landmark)
-        self.data = [hand]
+    def __init__(self, landmark: list, hand: list[int]) -> None:
+        self.landmark = landmark
+        self.hand = hand
+        self.data = []
 
-        for i in landmarks["landmark"]:
-            self.data.append(i['x'])
-            self.data.append(i['y'])
-            self.data.append(i['z'])
+        if self.landmark and self.hand != []:
+            for i in range(len(self.hand)):
+                _data = [self.hand[i]]
+                _landmark = MessageToDict(self.landmark[i])
+                for j in _landmark["landmark"]:
+                    _data.append(j['x'])
+                    _data.append(j['y'])
+                    _data.append(j['z'])
+
+                self.data.append(_data)
 
     def predict(self) -> np.array:
         """predicts the value
@@ -23,6 +30,10 @@ class number:
         Returns:
             np.array: pridected value
         """
-        output = self.__MODEL.predict(np.array(data))
 
-        return output
+        with open(site.getsitepackages()[-1] + "\\iwsh\\model\\number.pkl", "rb") as model:
+            MODEL = pickle.load(model)
+
+        if self.landmark and self.hand != []:
+            output = MODEL.predict(np.array(self.data))
+            return output.tolist()
